6cd0a186ffabfe981899c0370782dd243aeb2c00
viktor <alycasar@gmail.com>
2024-02-03 19:47:42 +0200

    create base.html, list.html, detail.html and canonical urls

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -18,7 +19,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Pablished'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -42,3 +44,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
